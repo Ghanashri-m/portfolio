45de019849c1f803e93a134056570f4681c5b278
Ghanashri-m <ghanashri.28@gmail.com>
2022-01-13 23:04:17 -0800

    Ghanashri Portfolio

---
+++ src/components/BlogData.js
@@ -0,0 +1,77 @@
+import numbers from '../assets/Images/know_Your_Numbers.png';
+import pokedex from '../assets/Images/pokedex.png';
+import quote from '../assets/Images/quote.png';
+import recipe from '../assets/Images/recipe.png';
+import geodemo from '../assets/Images/geodemo.png';
+import resumeImage from '../assets/Images/resumeImage.png';
+import ubereats from '../assets/Images/ubereats3.png';
+// import numbers from '../assets/Images/know_Your_Numbers.png';
+export const Blogs = [
+    {
+        id:1,
+        name:"Uber Eats Clone Using React JS and Node JS, hosted on AWS. ",
+        tags:["AWS","React JS","sNode JS", "MySQL"],
+        date:"10 Oct, 2021",
+        imgSrc: ubereats,
+        link:"https://www.youtube.com/watch?v=ujOusafm6U0&t=102s"
+    },
+    {
+        id:2,
+        name:"Resume generator application",
+        tags:["React","HTML", "CSS", "Javascript"],
+        date:"10 Sep, 2021",
+        imgSrc: resumeImage,
+        link:"https://www.youtube.com/watch?v=qGkaz5qvYGM"
+    },
+    {
+        id:3,
+        name:"Know Your Numbers, Fun math application",
+        tags:["HTML", "CSS", "Javascript"],
+        date:"6 July, 2021",
+        imgSrc: numbers,
+        link:"https://www.youtube.com/watch?v=EKPTuiW-xvo"
+    },
+    {
+        id:4,
+        name:"Pokemon Pokedex application",
+        tags:["HTML", "CSS", "Javascript"],
+        date:"26 May, 2021",
+        imgSrc: pokedex,
+        link:"https://www.youtube.com/watch?v=BHByb7x5ciA&t=5s"
+    },
+    {
+        id:5,
+        name:"Recipe Search Javascript Application",
+        tags:["HTML", "CSS", "Javascript"],
+        date:"3 Feb, 2021",
+        imgSrc: recipe,
+        link:"https://www.youtube.com/watch?v=3m4iwrLcPho"
+    },
+    {
+        id:6,
+        name:"Application to demonstrate Geolocation api",
+        tags:["HTML", "CSS", "Javascript"],
+        date:"5 Jan, 2021",
+        imgSrc: geodemo,
+        link:"https://www.youtube.com/watch?v=YxLB81w7SOA"
+
+    },
+   
+    {
+        id:7,
+        name:"Random Quote Machine",
+        tags:["HTML", "CSS", "Javascript"],
+        date:"16 May, 2021",
+        imgSrc: quote,
+        link:"https://www.youtube.com/watch?v=Bg_yr-umKDA"
+    },
+    // {
+    //     id:8,
+    //     name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+    //     tags:["react","framer-motion","styled-components"],
+    //     date:"26 May, 2021",
+    //     imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+    //     link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+    // },
+  
+] 
---
+++ src/components/WorkData.js
@@ -0,0 +1,31 @@
+export const Work = [
+    {
+        id:1,
+        name:"Bachelor of Engineering",
+        description:"Completed my Under Graduation degree from Visvesvaraya Technological University, GSSSIETW, with a total GPA of 3.69/4 and graduated in 2017",
+        tags:["Data Structure", "Analysis and Design of Algorithms", "Object-Oriented Programming", "Database Management Systems", "Web Technology"],
+        
+        demo:"https://agency-website-eta.vercel.app/",
+        // github:"https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id:2,
+        name:"Software Engineer",
+        description:"Worked as a Software Engineer at Capgemini Engineering (formerly Altran) from 2017 - 2021. During my tenure, worked on creating various user interfaces, framework components, database queries and back-end scripts to support full-featured and styled web applications",
+        tags:["ReactJS","Material UI", "JavaScript", "HTML", "CSS"],
+        
+        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+        // github:"https://github.com/codebucks27/react-job-listing-website"
+        
+    },
+    {
+        id:3,
+        name:"Master of Science in Computer Software Engineering",
+        description:"Pursuing masters since Fall 2021, at San Jose State University majoring in Computer Software Engineering with a specialization focused on enterprise distributed systems",
+        tags:["Design Patterns","Enterprise Software Platforms","Enterprise Distributed Systems"],
+        
+        demo:"http://devto-clone-codebucks27.vercel.app/",
+        // github:"https://github.com/codebucks27/devto-clone"
+    },  
+  
+] 
